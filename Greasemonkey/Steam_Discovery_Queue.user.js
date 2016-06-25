060cc94616b1706290be93d0c055a8829fa94907
onisuly <onisuly@gmail.com>
2016-06-25 22:03:02 +0800

    update

---
+++ Greasemonkey/Steam_Discovery_Queue.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Steam Discovery Queue
+// @description Auto go through Steam Discovery Queue
+// @namespace   oni
+// @include     http://store.steampowered.com/app/*
+// @include     http://store.steampowered.com/agecheck/*
+// @version     2016.06.25
+// @grant       none
+// ==/UserScript==
+
+
+if ( location.href.match(/http[s]?:\/\/store.steampowered.com\/agecheck\/app\/.+/) ) {
+    document.getElementsByName("ageYear")[0].value="1900";
+    DoAgeGateSubmit();
+}
+else if ( location.href.match(/http[s]?:\/\/store.steampowered.com\/app\/[0-9]+\/agecheck.+/) ) {
+    HideAgeGate();
+}
+else {
+    $J('#next_in_queue_form').submit();
+}
